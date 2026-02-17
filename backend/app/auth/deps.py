377484f5361a264214ffbd107a8c423a62d5ec98
njoroofficial <mosesn579@gmail.com>
2026-02-17 15:03:50 +0300

    feat(auth): implement get_current_user function for token validation and user retrieval

---
+++ backend/app/auth/deps.py
@@ -0,0 +1,36 @@
+from typing import Annotated
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from app.database_setup.schema import User
+from sqlmodel import Session, select
+from app.database_setup.database import get_session
+from app.auth.security import SECRET_KEY, ALGORITHM
+
+# This tells FastAPI that we expect the token in the "Authorization: Bearer" header
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)], session: Annotated[Session, Depends(get_session)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    try:
+        # 1. Decode the Token
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email: str = payload.get("sub")
+        if email is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    
+    # 2. Find the User in the DB
+    statement = select(User).where(User.email == email)
+    user = session.exec(statement).first()
+    
+    if user is None:
+        raise credentials_exception
+        
+    return user
