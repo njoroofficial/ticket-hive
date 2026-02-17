b49d10abe699aad3cd50533c25d5eea57564be1f
njoroofficial <mosesn579@gmail.com>
2026-02-17 14:50:50 +0300

    feat(user): implement user model

--- backend/app/database_setup/schema.py
+++ backend/app/database_setup/schema.py
@@ -2,7 +2,12 @@ import uuid
 from typing import Optional
 from sqlmodel import Field
 from app.models.event import EventBase
+from app.models.user import UserDBBase
 
 # it inherits from the event model
 class Event(EventBase, table=True):
+    id: Optional[uuid.UUID] = Field(default_factory=uuid.uuid4, primary_key=True)
+
+# User model
+class User(UserDBBase, table=True):
     id: Optional[uuid.UUID] = Field(default_factory=uuid.uuid4, primary_key=True)
